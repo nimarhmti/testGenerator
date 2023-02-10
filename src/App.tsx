dd249354b734fb576cc26d038109f5d1eccaf14f
nimarhmti <nimarahmati2000@gmail.com>
2023-02-10 18:13:09 +0330

    basic

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from "react";
 import AppProvider from "./AppProvider";
+import { Box } from "@mui/material";
+import { routes, routeModel } from "./config/routes";
+import { Routes, Route } from "react-router-dom";
 import "./style/globalStyle.css";
 
 const App = () => {
-  return <div>sui</div>;
+  return (
+    <Box>
+      <Routes>
+        {routes.map((item: routeModel, index: any) => (
+          <Route path={item.path} element={item.components} key={index} />
+        ))}
+      </Routes>
+    </Box>
+  );
 };
 
 export default AppProvider(App)();
