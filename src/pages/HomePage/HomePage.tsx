a8ccfaf96a1f4bd674d06b7e8f08ed3f1da35108
nimarhmti <nimarahmati2000@gmail.com>
2023-02-14 22:02:53 +0330

    sfsf

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,15 @@
+import { Container } from "@mui/material";
+import { Box } from "@mui/system";
 import React from "react";
 
 export const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <Box
+      borderRadius="100% 0% 100% 0% / 0% 9% 91% 100%"
+      height="100vh"
+      bgcolor="#FFF1D9"
+    >
+      <Container>jkdbnskn kjhasbvjks jsdnlknsdkl ljsnsd</Container>
+    </Box>
+  );
 };
