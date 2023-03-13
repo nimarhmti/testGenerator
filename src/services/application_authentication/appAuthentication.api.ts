24d625af249aaacc7bc9241982b49b38f62a9c15
nimarhmti <nimarahmati2000@gmail.com>
2023-03-13 19:40:15 +0330

    basics

--- src/services/application_authentication/appAuthentication.api.ts
+++ src/services/application_authentication/appAuthentication.api.ts
@@ -1,7 +1,6 @@
-import { async } from "q";
 import { api } from "../../config/axios";
 import type { authUserDataModel } from "./appAuthentication.interface";
 export const userAuthentication = async (payload: authUserDataModel) => {
-  const response = await api.post("", payload);
+  const response = await api.post("/register", payload);
   return response;
 };
